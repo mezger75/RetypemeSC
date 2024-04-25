5a7c4b5b1a96d7835d240c9fc8ff38d995a76fc1
apozdniakov <t0lia.gm@gmail.com>
2024-04-25 23:25:39 +0200

    deploy to scroll sepolia

--- script/GamingContract.s.sol
+++ script/GamingContract.s.sol
@@ -1,11 +1,10 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.20;
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
 
 import "forge-std/Script.sol";
 import "../src/GamingContract.sol";
 
-// forge script script/GamingContract.s.sol:GamingContractScript --rpc-url amoy_polygon --broadcast -vvvv
-// forge verify-contract 0xb3Aa754f1664719489fb10e0c5F9B98D8AC232b9 src/GamingContract.sol:GamingContract --rpc-url amoy_polygon
 contract GamingContractScript is Script {
     function run() external {
         uint256 deployedPrivateKey = vm.envUint("PRIVATE_KEY");
