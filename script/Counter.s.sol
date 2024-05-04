fadddea1e8fa7e0354291c98545a89c7a172b4eb
apozdniakov <t0lia.gm@gmail.com>
2024-05-04 11:39:20 +0200

    migrate to create2 for deployment

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,11 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import {Counter} from "../src/Counter.sol";
 
 contract CounterScript is Script {
     function setUp() public {}
 
-    function run() public {
-        vm.broadcast();
+    function run() external {
+        bytes32 salt = keccak256(abi.encodePacked(address(this), vm.envUint("PRIVATE_KEY")));
+
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+        new Counter{salt: salt}();
+        vm.stopBroadcast();
     }
+
 }
